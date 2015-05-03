1f9287d0729f8e352599216efd19f533e4293d08
Lyn Muldrow <lyn.muldrow@gmail.com>
2015-05-03 15:39:34 -0700

    tried new js method, unworking

--- main.js
+++ main.js
@@ -1,36 +1,47 @@
+window.onload = function() {
+    var fortuneSpan     = document.getElementById("fortuneText");
+    var submitButton  	= document.getElementById('submit');
+    var oldFortuneIndex = -1;
+    var newFortuneIndex = -1;
+	var fortunearray 	= [
+	{'text': "You shouldn't quit your day job."},
+	{'text':"You're like totally an imposter. Get a life, dude."},
+	{'text':"I tried to write something nice but I can't even."},
+	{'text':"You go, Glen Coco!"},
+	{'text':"Nothing is impossible, the word itself says ‘I’m possible’!"},
+	{'text':"A guilty conscience needs to confess. A work of art is a confession."},
+	{'text':"The visionary is the only realist."},
+	{'text':"A day without sunshine is like, you know, night."},
+	{'text':"Whatever you are, be a good one."},
+	{'text':"Every hero becomes a bore at last."},
+	{'text':"You are so fetch. Like, seriously."},
+	{'text':"Hipster Ariel is life. She was cool before you were."},
+	{'text':"Dude, where's your raybans?"},
+	{'text':"If you're not composting, you fail at life."},
+	{'text':"You drink Starbucks- that is your downfall."},
+	{'text':"Life is hard; it’s harder if you’re stupid."},
+	{'text':"Don’t use a big word where a diminutive one will suffice."},
+	{'text':"When shit brings you down, just say ‘fuck it’, and eat yourself some motherfucking candy."},
+	{'text':"If you go home with somebody and they don’t have books, don’t fuck them!"},
+	{'text':"Sometimes the sins you haven’t committed are all you have left to hold onto."},
+	{'text':"We are mature in one realm, childish in another."},
+	{'text':"I am so clever that sometimes I don’t understand a single word of what I am saying."},
+	{'text':"A guilty conscience needs to confess. A work of art is a confession."},
+	{'text':"Future hipsters will love you ironically."},
+	{'text':"Part of growing up is not waiting in line at a hipster breakfast restaurant."},
+	{'text':"Nobody hates hipsters more than hipsters."},
+	{'text':"There might be a need to fabricate your own beauty paradigms."}
+	];
 
-function getMessage() {
-var messagearray = new Array(
-"You shouldn't quit your day job.",
-"You're like totally an imposter. Get a life, dude.",
-"I tried to write something nice but I can't even.",
-"You go, Glen Coco!",
-"Nothing is impossible, the word itself says ‘I’m possible’!",
-"A guilty conscience needs to confess. A work of art is a confession.",
-"The visionary is the only realist.",
-"A day without sunshine is like, you know, night.",
-"Whatever you are, be a good one.",
-"Every hero becomes a bore at last.",
-"You are so fetch. Like, seriously.",
-"Hipster Ariel is life. She was cool before you were.",
-"Dude, where's your raybans?",
-"If you're not composting, you fail at life.",
-"You drink Starbucks- that is your downfall.",
-"Life is hard; it’s harder if you’re stupid.",
-"Don’t use a big word where a diminutive one will suffice.",
-"When shit brings you down, just say ‘fuck it’, and eat yourself some motherfucking candy.",
-"If you go home with somebody and they don’t have books, don’t fuck them!",
-"Sometimes the sins you haven’t committed are all you have left to hold onto.",
-"We are mature in one realm, childish in another.",
-"I am so clever that sometimes I don’t understand a single word of what I am saying.",
-"A guilty conscience needs to confess. A work of art is a confession.",
-"Future hipsters will love you ironically.",
-"Part of growing up is not waiting in line at a hipster breakfast restaurant.",
-"Nobody hates hipsters more than hipsters.",
-"There might be a need to fabricate your own beauty paradigms."
-);
+   function nextFortune() {
+        while (newFortuneIndex == oldFortuneIndex) {
+            newFortuneIndex = Math.floor(Math.random() * fortunearray.length);
+        }
 
-   var sendmessages = messagearray[Math.floor(Math.random() * messagearray.length)];
-   document.getElementById("message").innerHTML=random;
+        fortuneSpan.innerHTML  = fortunearray[newFortuneIndex].text; //make HTML's fortuneText random quote
+
+ 		oldQuoteIndex = newQuoteIndex;
+    }
+
+	submitButton.onclick = nextFortune;
 }
-
